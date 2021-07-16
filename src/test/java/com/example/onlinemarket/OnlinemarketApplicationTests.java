b9ce136870fec04d00ff473dd72cc00efd1ae536
nik61088 <nik61088@gmail.com>
2021-07-16 19:07:35 +0300

    Initial commit

---
+++ src/test/java/com/example/onlinemarket/OnlinemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinemarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
